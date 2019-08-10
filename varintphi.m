d54587a5f1ebcf8507b4415990145f80921d89b6
Yujie Zheng <yjzheng@Messi.sunet>
2019-08-10 13:34:51 -0700

    Just commit indexed/tracked

--- varintphi.m
+++
@@ -1,6 +0,0 @@
-function varphi=varintphi(rho)
-% function to compute var(\phi) when L=1
-% phi here is interferometric phase
-varphi=pi^2/3-pi*asin(rho)+(asin(rho)).^2-0.5*dilog(1-rho.^2);
-
-return
